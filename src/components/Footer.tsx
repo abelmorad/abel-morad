c1538500dbd76e967e9ecbfbcb1a0d361d038d7e
abelmorad <abelmorad97@gmail.com>
2023-07-30 14:59:27 +0800

    Feat: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
-
-
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
-  )
+    <footer className="flex items-center place-content-center text-xs">
+      <p className="absolute bottom-0 mb-3">Copyright &copy; {currentYear} | Built by Sharhabeel Morad</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
