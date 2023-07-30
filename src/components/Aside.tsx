a9438b8e2b9eabc8f1254a6708744fbf9edd8a9b
abelmorad <abelmorad97@gmail.com>
2023-07-31 06:06:47 +0800

    Style: centered

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -3,10 +3,10 @@ import {BsFacebook, BsGithub, BsLinkedin} from "react-icons/bs"
 
 function Aside() {
   return (
-    <aside className="flex flex-col items-start gap-5 aside">
-      <Link to="/" className="text-2xl"><BsGithub /></Link>
-      <Link to="/" className="text-2xl"><BsLinkedin /></Link>
-      <Link to="/" className="text-2xl"><BsFacebook /></Link>
+    <aside className="flex flex-col items-center place-content-center gap-5 aside">
+      <Link to="/" ><BsGithub size="1.5rem" /></Link>
+      <Link to="/" ><BsLinkedin size="1.5rem" /></Link>
+      <Link to="/" ><BsFacebook size="1.5rem" /></Link>
     </aside>
   )
 }
