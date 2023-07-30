af7ef502a285783f4f10048ac8fb15d51641a9a4
abelmorad <abelmorad97@gmail.com>
2023-07-30 14:58:36 +0800

    Feat: aside

---
+++ src/components/Aside.tsx
@@ -0,0 +1,12 @@
+
+function Aside() {
+  return (
+    <aside className="flex flex-col items-start gap-5 aside">
+          <ion-icon size="large" name="logo-linkedin"></ion-icon>
+          <ion-icon size="large" name="logo-facebook"></ion-icon>
+          <ion-icon size="large" name="logo-github"></ion-icon>
+    </aside>
+  )
+}
+
+export default Aside
