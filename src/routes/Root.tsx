8e43f8ce17a79bd06dcc44647a18277140a1f5cf
abelmorad <abelmorad97@gmail.com>
2023-07-30 15:01:34 +0800

    Refactor

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import Landing from "../pages/Landing";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -8,7 +8,6 @@ function Root() {
   return (
     <>
       <Header />
-      <Landing />
       <Outlet />
       <Footer />
     </>
