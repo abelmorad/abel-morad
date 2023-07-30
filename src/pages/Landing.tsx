c386e6a049d64e9c52fccbe48338e9e78caebe51
abelmorad <abelmorad97@gmail.com>
2023-07-31 06:58:35 +0800

    Style: update

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,8 +4,7 @@ import Aside from "../components/Aside";
 function Landing() {
   return (
     <section className="flex flex-col h-screen items-center place-content-center">
-      <div className="grid grid-flow-col-dense gap-5 laptop:px-10 px-3">
-        <Aside />
+      <div className="grid grid-flow-col-dense gap-5 laptop:px-10 px-7">
         <section className="flex flex-col items-start place-content-center tablet:px-20 laptop:px-40 desktop:px-72">
           <p className="font-light">Hi, I am</p>
           <h1 className="text-4xl font-semibold">Sharhabeel Morad</h1>
@@ -22,6 +21,7 @@ function Landing() {
             Contact Me
           </Link>
         </section>
+        <Aside />
       </div>
     </section>
   );
