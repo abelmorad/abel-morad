160de59d3d8972d53e079df7099b75f971c8b45a
abelmorad <abelmorad97@gmail.com>
2023-07-30 16:08:34 +0800

    Feat: replaced ionicicons with reacticons

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+import {BsFacebook, BsGithub, BsLinkedin} from "react-icons/bs"
 
 function Aside() {
   return (
     <aside className="flex flex-col items-start gap-5 aside">
-          <ion-icon size="large" name="logo-linkedin"></ion-icon>
-          <ion-icon size="large" name="logo-facebook"></ion-icon>
-          <ion-icon size="large" name="logo-github"></ion-icon>
+      <Link to="/" className="text-2xl"><BsGithub /></Link>
+      <Link to="/" className="text-2xl"><BsLinkedin /></Link>
+      <Link to="/" className="text-2xl"><BsFacebook /></Link>
     </aside>
   )
 }
