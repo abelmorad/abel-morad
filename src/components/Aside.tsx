8b62aa265997eec7053cbeeac227c165db299dae
abelmorad <abelmorad97@gmail.com>
2023-07-31 06:59:07 +0800

    Style: update

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -3,7 +3,7 @@ import {BsFacebook, BsGithub, BsLinkedin} from "react-icons/bs"
 
 function Aside() {
   return (
-    <aside className="flex flex-col items-center place-content-center gap-5 aside">
+    <aside className="flex flex-col items-center place-content-center gap-5 absolute right-0 bottom-36 top-36 aside">
       <Link to="/" ><BsGithub size="1.5rem" /></Link>
       <Link to="/" ><BsLinkedin size="1.5rem" /></Link>
       <Link to="/" ><BsFacebook size="1.5rem" /></Link>
