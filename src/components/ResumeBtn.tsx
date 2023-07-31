06e774b71692dc1bae31a86b86c604883cc40112
abelmorad <abelmorad97@gmail.com>
2023-07-31 09:16:58 +0800

    Style: button mobile size

--- src/components/ResumeBtn.tsx
+++ src/components/ResumeBtn.tsx
@@ -6,7 +6,7 @@ function ResumeBtn() {
     <Link
         to={Resume}
         target="_blank"
-        className="text-lg font-medium bg-transparent px-5 py-1 vaporwave-border hover:scale-105"
+        className="text-lg font-medium bg-transparent px-5 py-1 mobile:px-7 mobile:py-3 vaporwave-border hover:scale-105"
       >
         Resume
       </Link>
