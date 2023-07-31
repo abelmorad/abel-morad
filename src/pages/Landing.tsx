591372bb981223f3ed32c34573ddd7d604f43709
abelmorad <abelmorad97@gmail.com>
2023-07-31 08:25:24 +0800

    Style: typography changes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -6,17 +6,17 @@ function Landing() {
     <section className="flex flex-col h-screen items-center place-content-center">
       <div className="grid grid-flow-col-dense gap-5 laptop:px-10 px-7">
         <section className="flex flex-col items-start place-content-center tablet:px-20 laptop:px-40 desktop:px-96">
-          <p className="font-light">Hi, I am</p>
+          <p className="font-normal">Hi, I am</p>
           <h1 className="text-4xl font-semibold">Sharhabeel Morad</h1>
           <h2 className="font-extralight">Front-end Developer | React </h2>
-          <p className="mt-3 font-light">
+          <p className="mt-2 font-normal">
             I am a self-taught front-end developer learning various technologies
             to develop my skills and be a versatile web developer to build
             real-world projects that impact people.
           </p>
           <Link
             to="/"
-            className="flex text-lg font-medium bg-transparent px-5 py-1 mt-4 place-content-center vaporwave-border hover:scale-105"
+            className="flex text-lg font-medium bg-transparent px-5 py-1 mt-8 place-content-center vaporwave-border hover:scale-105"
           >
             Contact Me
           </Link>
