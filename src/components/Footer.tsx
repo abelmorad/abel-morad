65608c8ede7989991262d709f5493c9fad361905
abelmorad <abelmorad97@gmail.com>
2023-08-02 16:19:17 +0800

    Style: update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex items-center place-content-center text-xs py-3 fixed bottom-0 w-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+    <footer className="flex items-center place-content-center text-xs py-3 w-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
       <p>Copyright &copy; {currentYear} | Built by Sharhabeel Morad</p>
     </footer>
   );
