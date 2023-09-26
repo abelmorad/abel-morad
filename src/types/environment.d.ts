160c43687ec8209fe970c9108f1dd0be0a3feae5
Sharhabeel B. Morad <abelmorad97@gmail.com>
2023-09-26 11:12:52 +0800

    Update

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,9 +3,9 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      REACT_APP_SERVICE_ID: string;
-      REACT_APP_TEMPLATE_ID: string;
-      REACT_APP_API_KEY: string;
+      REACT_APP_SERVICE_ID: any;
+      REACT_APP_TEMPLATE_ID: any;
+      REACT_APP_API_KEY: any;
       ENV: "test" | "dev" | "prod";
     }
   }
