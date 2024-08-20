22094b73e616762eb37aefcfd1e1be77330177a9
Sharhabeel B. Morad <abelmorad97@gmail.com>
2024-08-20 15:51:27 +0800

    update page

---
+++ src/pages/Services.tsx
@@ -0,0 +1,14 @@
+function Services() {
+  return (
+    <section id="services" className="flex flex-col px-6 h-screen place-content-center tablet:px-20 laptop:px-40 desktop:px-96 ">
+    <section className="flex flex-col">
+      <h1 className="text-3xl font-semibold mb-5">Services</h1>
+    </section>
+    <section className="flex flex-wrap pt-10 place-content-center tablet:place-content-start">
+      
+    </section>
+  </section>
+  )
+}
+
+export default Services
